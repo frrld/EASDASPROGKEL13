524dc6259de66668d009d113030c90f035a2ae60
frrld <muh.farrel.danendra@gmail.com>
2024-06-26 09:12:37 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * -----------------------------------------------------
+ * ES234211 - Programming Fundamental
+ * Genap - 2023/2024
+ * Group Capstone Project: Snake and Ladder Game
+ * -----------------------------------------------------
+ * Class    : C
+ * Group    : XX
+ * Members  :
+ * 1. Student ID - Full Name
+ * 2. Student ID - Full Name
+ * 3. Student ID - Full Name
+ * ------------------------------------------------------
+ */
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        SnakeAndLadder g1 = new SnakeAndLadder(100);
+        g1.play();
+    }
+}
